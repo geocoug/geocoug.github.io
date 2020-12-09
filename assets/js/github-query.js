7103cdcb85f18509c5a210842b778bfa7de4bac9
geocoug <grantcaleb22@gmail.com>
2020-12-09 11:01:05 -0800

    project imgs & links in new tab

--- assets/js/github-query.js
+++ assets/js/github-query.js
@@ -16,7 +16,7 @@ jQuery.fn.loadRepositories = function(username) {
             if (this.name != (username.toLowerCase()+'.github.com')) {
                 if (this.name == 'Task-Tracker') {}
 	            else {
-                    list.append('<dt><strong><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></strong> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+                    list.append('<dt><strong><a href="'+ (this.homepage?this.homepage:this.html_url) +'" target="_blank">' + this.name + '</a></strong> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
                     if (this.description == null) {
                         list.append('<dd style="margin-bottom:20px;"></dd>')
                     }
